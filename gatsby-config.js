54918de7f5bbde6e03274eb6f99d6903a8ee14c6
eleline <ggggepicstar@gmail.com>
2019-12-15 23:07:00 +0900

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ require(`dotenv`).config({
 
 module.exports = {
   siteMetadata: {
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
+    siteTitleAlt: `ELELINE Portfolio`,
   },
   plugins: [
     {
@@ -20,9 +20,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Cara - @lekoarts/gatsby-theme-cara`,
-        short_name: `Cara`,
-        description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+        name: `ELELINE`,
+        short_name: `ELELINE`,
+        description: `Portfolio for ELELINE`,
         start_url: `/`,
         background_color: `#141821`,
         theme_color: `#f6ad55`,
